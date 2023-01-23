3b777923da507ffeb19d419f3b9e90c396540d93
herianwar78 <114667621+herianwar78@users.noreply.github.com>
2023-01-23 20:30:39 +0700

    Update no3.py

--- no3.py
+++ no3.py
@@ -6,7 +6,7 @@ def tombol_ditekan():
     label2 = tkinter.Label(main_window, text="tombol ditekan")
     label2.pack()
 
-label = tkinter.Label(main_window, text="Hello world \n ini adalah GUI sederhana \n menggunakan python")
+label = tkinter.Label(main_window, text="Hellow world \n ini adalah GUI sederhana \n menggunakan python")
 tombol = tkinter.Button(main_window, text="Tombol", command_=_tombol_ditekan)
 label.pack()
 tombol.pack()
